5d067567261ecd306f69da66727e4506b35436ab
aagusvelazquez <velazquezagustinamailen@gmail.com>
2023-11-10 15:33:24 -0300

    Cree barra de navegación y contenido principal para dispositivos e tamaños pequeños o pantallas grandes.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+document.getElementById('anio').innerHTML = new Date().getFullYear();
+document.getElementById('añoCampusDesktop').innerHTML = new Date().getFullYear();
+document.getElementById('añoCampusMobile').innerHTML = new Date().getFullYear();
 document.getElementById('login-btn').addEventListener('click', () => {
     var usuario = document.getElementById("email").value;
     var contraseña = document.getElementById("password").value;
@@ -17,3 +20,34 @@ const sidebar = document.querySelector("aside");
 sidebarBtn.addEventListener('click', () => {
     document.body.classList.toggle('active');
 })
+
+
+const btnMenuMobile = document.getElementById('btnMenuMobile');
+const dropdown = document.querySelector('.dropdown__wrapper--fade-in');
+
+btnMenuMobile.addEventListener('click', () => {
+    dropdown.classList.remove('none');
+    dropdown.classList.toggle('hide');
+});
+
+document.addEventListener("click", (event) => {
+    const isClickInsideDropdown = dropdown.contains(event.target);
+    const isButtonClicked = button.contains(event.target);
+    if (!isClickInsideDropdown && !isButtonClicked) {
+        dropdown.classList.add('hide');
+        dropdown.classList.add('dropdown__wrapper--fade-in');
+    }
+});
+
+const btnMenu = document.querySelector('.btnMenu');
+const closeButton = document.querySelector('.close');
+const menu = document.querySelector('.menu');
+
+btnMenu.addEventListener('click', () => {
+    menu.classList.remove('none');
+    menu.classList.remove('hide');
+});
+
+closeButton.addEventListener('click', () => {
+    menu.classList.add('hide');
+});
